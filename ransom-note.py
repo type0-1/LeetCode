44c797a37face9ba6e32a28cc4f47533c4108c02
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-01-29 17:23:35 +0000

    Create ransom-note.py

---
+++ ransom-note.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for c in ransomNote:
+            if c in magazine:
+                ransomNote = ransomNote.replace(c, "", 1)
+                magazine = magazine.replace(c, "", 1)
+        if len(ransomNote) == 0:
+            return True
+        return False
