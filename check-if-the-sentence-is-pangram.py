10b98ad6f3b5c1e728475d9da334420e95300db2
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-01-30 08:38:22 +0000

    Create check-if-the-sentence-is-pangram.py

---
+++ check-if-the-sentence-is-pangram.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        a = "abcdefghijklmnopqrstuvwxyz"
+        for i in sentence:
+            if i in a:
+                a = a.replace(i, "", 1)
+        if len(a) == 0:
+            return True
+        return Fals
