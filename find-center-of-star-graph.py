4e75422c7308f1740bf59f1637d2fd6286c05e6a
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-02-02 10:28:04 +0000

    Create find-center-of-star-graph.py

---
+++ find-center-of-star-graph.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        s = ""
+        for l in edges:
+            for i in l:
+                s += str(i) + " "
+        s = s.split()
+        for i in s:
+            if s.count(i) > 1:
+                return int(i)
