0af6d0a581c121e3b837c47994fff1a4a84c1c3d
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-02-08 14:55:44 +0000

    Create intersection-of-two-arrays.py

---
+++ intersection-of-two-arrays.py
@@ -0,0 +1,7 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result = []
+        for n in nums1:
+            if n in nums2 and result.count(n) == 0:
+                result.append(n)
+        return result
