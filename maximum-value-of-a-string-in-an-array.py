e5b5bc599b2887f8a4934ec1d7b49420b827957f
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-02-08 09:24:24 +0000

    Create maximum-value-of-a-string-in-an-array.py

---
+++ maximum-value-of-a-string-in-an-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximumValue(self, strs: List[str]) -> int:
+        biggest = 0
+        for data in strs:
+            if data.isnumeric():
+                if int(data) > biggest:
+                    biggest = int(data)
+            elif data.isalnum():
+                if len(data) > biggest:
+                    biggest = len(data)
+        return biggest
