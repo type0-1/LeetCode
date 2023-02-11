fe49919c891567c0c64cfebe7108759b5da52d02
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-02-11 20:46:46 +0000

    Create a-number-after-a-double-reversal.py

---
+++ a-number-after-a-double-reversal.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isSameAfterReversals(self, num: int) -> bool:
+        if num < 10:
+            check = str(int(num))[::-1]
+            return check[::-1] == str(num)
+        else:
+            check = str(int(num))[::-1].strip("0")
+            return str(num) == check[::-1]
+        
