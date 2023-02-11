c95bc208d9a472c965d4545685b3dc17db378363
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-02-11 20:14:32 +0000

    Create count-the-digits-that-divide-a-number.py

---
+++ count-the-digits-that-divide-a-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countDigits(self, num: int) -> int:
+        if num < 10:
+            return 1
+        else:
+            n = str(num)
+            count = 0
+            for i in n:
+                if num % int(i) == 0:
+                    count += 1
+            return count
