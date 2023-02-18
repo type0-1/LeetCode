266884b70eb8f0ab60759062e9498eb10e3a3f9b
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-02-18 22:03:44 +0000

    Create plus-one.py

---
+++ plus-one.py
@@ -0,0 +1,5 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        stringD = "".join(str(n) for n in digits)
+        stringD = str(int(stringD) + 1)
+        return [int(n) for n in stringD]
