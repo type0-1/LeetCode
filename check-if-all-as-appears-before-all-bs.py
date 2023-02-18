fefc0c41053cd3f583949587ac38b1853b8ed3b2
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-02-18 21:12:35 +0000

    Create check-if-all-as-appears-before-all-bs.py

---
+++ check-if-all-as-appears-before-all-bs.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkString(self, s: str) -> bool:
+        i = 0
+        a = 0
+        if "b" in s:
+            b = s.index("b")
+            while i < len(s):
+                if s[i] == "a":
+                    a = i
+                else:
+                    b = i
+                if a > b:
+                    return False
+                i += 1
+        else:
+            pass
+        return True
