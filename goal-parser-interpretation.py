4a9994b4545e3a7d35c8e0e94ef0f33cd0a3afaa
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-02-18 20:25:09 +0000

    Create goal-parser-interpretation.py

---
+++ goal-parser-interpretation.py
@@ -0,0 +1,7 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        command = command.replace("()", "o")
+        for l in command:
+            if l == "(" or l == ")":
+                command = command.replace(l, "")
+        return command
