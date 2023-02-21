8dc00c4f34361c0d4d03095a23ec8e5f67a4a905
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-02-21 10:01:38 +0000

    Create third-maximum-number.py

---
+++ third-maximum-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        new = []
+        for n in nums:
+            if n not in new:
+                new.append(n)
+        if len(new) <= 2:
+            return max(new)
+        else:
+            return new[2]
