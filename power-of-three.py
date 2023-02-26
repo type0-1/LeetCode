e7f1dedbc64ffda9ec557f1f6509ac47bf0e802b
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-02-26 14:26:39 +0000

    Create power-of-three.py

---
+++ power-of-three.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        for i in range(n + 1):
+           power = 3 ** i
+           if power > n:
+               return False
+           else:
+               if power == n:
+                   return True
