10e425e60af0fca221d3a1426053c7d57a6949fa
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-02-26 23:21:36 +0000

    Create sort-the-students-by-their-kth-score.py

---
+++ sort-the-students-by-their-kth-score.py
@@ -0,0 +1,5 @@
+class Solution:
+    def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
+        new = [(sc[k], sc) for sc in score]
+        new.sort(reverse=True)
+        return [scores[1] for scores in new]
