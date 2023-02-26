1a01b9c2204faf7b46517d39345bd780f762a2b1
Samson Oloruntola <114999736+type0-1@users.noreply.github.com>
2023-02-26 01:13:06 +0000

    Create add-digits.py

---
+++ add-digits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num > 9:
+            string = str(num)
+            total = 0
+            for n in string:
+                total += int(n)
+            num = total
+        return num
